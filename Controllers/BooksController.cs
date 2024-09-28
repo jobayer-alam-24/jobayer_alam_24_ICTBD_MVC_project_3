6c078857e3bff3db7bba8c954e0d6a7ab9bb02af
Jobayer Alam <sheikhnayamul123@gmail.com>
2024-09-28 23:34:20 +0600

    updated

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using jobayer_alam_24_ICTBD_MVC_project_3.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -19,7 +20,17 @@ namespace jobayer_alam_24_ICTBD_MVC_project_3.Controllers
 
         public IActionResult Data()
         {
-            return View();
+            Books books = new Books()
+            {
+                BookID = 1,
+                Title = "To Kill a Mockingbird",
+                Author = "Harper Lee",
+                Genre = "Fiction",
+                Publisher = "J.B. Lippincott & Co.",
+                ISBN = "978-0-06-112008-4"
+            };
+
+            return View(books);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Books.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jobayer_alam_24_ICTBD_MVC_project_3.Models
+{
+    public class Books
+    {
+        public int BookID {get; set;}
+        public string Title {get; set;}
+        public string  Author  {get; set;}
+        public string Genre {get; set;}
+        public string Publisher {get; set;}
+        public string ISBN {get; set;}
+    }
+}
