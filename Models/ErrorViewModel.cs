fa4c6a2a6886f6f800043ae36e1a110dc240edbb
Jobayer Alam <sheikhnayamul123@gmail.com>
2024-09-28 21:41:08 +0600

    working

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jobayer_alam_24_ICTBD_MVC_project_3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
